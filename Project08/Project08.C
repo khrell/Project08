ff01a2e19a8b8647fed361298cbfe3af4ab67518
riptorn01@gmail.com <Trinity1>
2016-10-23 10:09:36 -0500

    Rev 6 working on sorting order

--- Project08/Project08.C
+++ Project08/Project08.C
@@ -18,30 +18,47 @@ int total_votesb(int b[]);
 int total_votesc(int c[]);
 int total_votesd(int d[]);
 int print_chart(int precint[], int a[], int b[], int c[], int d[], double totala, double totalb, double totalc, double totald, int suma, int sumb, int sumc, int sumd);
-//int winner(double totala, double totalb, double totalc, double totald);
+int winner(double totala, double totalb, double totalc, double totald);
+void order(double *smp, double *lgp);
 
 int main()
 {
+	/*intializes the arrays*/
 	int a[] = { 192, 147,186,114,267 };
 	int b[] = { 48, 90, 12, 21, 13 };
 	int c[] = { 206, 312, 121, 408, 382 };
 	int d[] = { 37, 21, 38, 39, 29 };
 	int precint[] = { 1,2,3,4,5 };
+	
+	/*sums the candidates total votes by precint*/
 	int suma = total_votesa(a);
 	int sumb = total_votesb(b);
 	int sumc = total_votesc(c);
 	int sumd = total_votesd(d);
-	double total = suma + sumb + sumc + sumd;
+	double total = suma + sumb + sumc + sumd; //gets the total votes for all candidates
 
+	/*gets percentages of the candidates votes*/
 	double totala = (100 * suma) / total;
 	double totalb = (100 * sumb) / total;
 	double totalc = (100 * sumc)/ total;
 	double totald = (100 * sumd) / total;
 
 	
+	
+	/*calls the method and prints the chart*/
 	print_chart(precint,a,b,c,d,totala,totalb,totalc,totald,suma,sumb,sumc,sumd);
-	//int winner(double totala, double totalb, double totalc, double totald);
-
+	
+	/*calls the method and prints the winner of the election*/
+	winner(totala,totalb,totalc,totald);
+	
+	/*orders the percentages*/
+	order(&totala, &totalb);
+	order(&totala, &totalc);
+	order(&totala, &totald);
+	order(&totalb, &totalc);
+	order(&totalb, &totald);
+	order(&totalc, &totald);
+	printf("The numbers are: %.2f %.2f %.2f %.2f\n", totala, totalb, totalc, totald);
 
 	return;
 }
@@ -106,14 +123,44 @@ int print_chart(int precint[], int a[], int b[], int c[], int d[], double totala
 	}
 	printf("____________________________________________________________________\n");
 	printf("Total\t %6d\t\t %6d\t\t %6d\t\t %6d \n",suma,sumb,sumc,sumd);
-	printf("Percentage| %.1f%%\t   %.1f%%\t\t   %.1f%%\t   %.1f%% \n", totala, totalb, totalc, totald);
+	printf("Percentage| %.1f%%\t   %.1f%%\t\t   %.1f%%\t   %.1f%% \n\n", totala, totalb, totalc, totald);
 
 } 
 
-/*int winner(double totala, double totalb, double totalc, double totald)
+int winner(double totala, double totalb, double totalc, double totald)
 {
 	
+	if (totala > 50)
+	{
+		printf("The winner is Candidate A!\n\n");
+	}
+	else if (totalb > 50)
+	{
+		printf("The winner is Candidate B!\n\n");
+	}
+	else if (totalc > 50)
+	{
+		printf("The winner is Candidate C!\n\n");
+	}
+	else if (totald > 50)
+	{
+		printf("The winner is Candidate D!\n\n");
+	}
+			
+	else
+	{
+		printf("There will be a runoff between the two highest !\n");
+		
+	}
+}
+
+void order(double *smp, double *lgp)
+{
 	
-}*/
+	double temp = 0;
 
+	if (*smp > *lgp) 
+		temp = *smp;
+		*smp = *lgp; *lgp = temp;
 
+}
